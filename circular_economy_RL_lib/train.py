25aed53bb782304bb4fa026f97aca0adca6bc1de
ColdRiver <mailbox.rishit@gmail.com>
2026-06-24 18:02:49 +0530

    Update train.py

--- circular_economy_RL_lib/train.py
+++ circular_economy_RL_lib/train.py
@@ -3,6 +3,12 @@ import os
 import multiprocessing
 from trainer import BilevelTrainer
 from utils import create_logger
+import os
+import sys
+
+# Prevent OpenMP and MKL thread-pool deadlocks between PyTorch and TensorFlow
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
 
 # Ensure PYTHONPATH handles local modules correctly
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
